f69faba058695c1daf580854a0af6abf3610c5ba
tandf_PC <tandf@live.cn>
2018-10-11 00:12:08 +0800

    templates

--- js/igemscript.js
+++ js/igemscript.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
     hq = document.getElementById("HQ_page");
     hq.id = '';
     $(".mw-body").css("background", "transparent");
+    $(".mw-body").css("margin-top", "-12px");
 });
 })(jQuery);
